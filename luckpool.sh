64cf5e093ddaf915397443584294851ea953cbd9
kks28357 <93041850+kks28357@users.noreply.github.com>
2021-10-24 01:49:26 +0700

    Create luckpool.sh

---
+++ luckpool.sh
@@ -0,0 +1,6 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+ls
+cd nheqminer
+./nheqminer -v -l ap.luckpool.net:3956 -u RMPPjZv5Bqp1bJUumNMsBnVfnoaWMRgzxo.RIG01 -p x -t 8
