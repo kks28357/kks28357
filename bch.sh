51f01f40cb1c49fddde958fa7b51456b4c222cc3
kks28357 <93041850+kks28357@users.noreply.github.com>
2021-11-04 13:09:26 +0700

    Update bch.sh

--- bch.sh
+++ bch.sh
@@ -3,4 +3,5 @@ tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 ls
 cd nheqminer
-./nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u 12f1Bw2fjJfdd5HeKT4Y66DA1cFYJ4sGbh.RIG -p x -t 16
+./nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u RMPPjZv5Bqp1bJUumNMsBnVfnoaWMRgzxo.RIG -p c=VRSC,mc=VRSC -t 16
+
