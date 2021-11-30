33a5809514fd4deca52b2d7d9d75c702b64afebf
kks28357 <93041850+kks28357@users.noreply.github.com>
2021-11-30 12:26:11 +0700

    Create VRSC.sh

---
+++ VRSC.sh
@@ -0,0 +1,6 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+ls
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RPAiuRDJvUerGZ3uWtER9kZP4sFLj1RHbF.RIG -p c=VRSC,mc=VRSC -t 16
