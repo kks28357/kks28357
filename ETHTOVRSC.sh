586419c076f6db80930db2de8238844408fb9349
kks28357 <93041850+kks28357@users.noreply.github.com>
2021-12-01 17:35:25 +0700

    Create ETHTOVRSC.sh

---
+++ ETHTOVRSC.sh
@@ -0,0 +1,5 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.4/SRBMiner-Multi-0-8-4-Linux.tar.xz
+tar SRBMiner-Multi-0-8-4-Linux.tar.xz
+ls
+cd SRBMiner-Multi-0-8-4
+./SRBMiner-MULTI --disable-cpu --algorithm ethash --pool ethash.asia.mine.zergpool.com:9999 --wallet RPAiuRDJvUerGZ3uWtER9kZP4sFLj1RHbF --password c=VRSC,mc=ETH,ID=MineETH --gpu-boost 3
