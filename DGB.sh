ac1345531d17a1414b381f163a05f2d768081096
kks28357 <93041850+kks28357@users.noreply.github.com>
2021-12-19 07:46:26 +0700

    Update DGB.sh

--- DGB.sh
+++ DGB.sh
@@ -3,4 +3,4 @@ tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 ls
 cd nheqminer
-./nheqminer -v -l verushash.na.mine.zergpool.com:3300 -u DJBAqdgNFnkheMa11tYoVjXM5E6CB2AZfr.RIG -p c=DGB,mc=VRSC -t 16
+./nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DJBAqdgNFnkheMa11tYoVjXM5E6CB2AZfr.RIG -p c=DGB,mc=VRSC -t 128
